59f95c566aad58eb2cf498ec75804c6b80cb604b
Oluwavictor <46841607+Oluwavictor@users.noreply.github.com>
2023-03-31 09:10:14 +0100

    initial commits

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    A script that that sends a Request and displays the body of
+    the response
+"""
+
+
+if __name__ == "__main__":
+    import sys
+    from urllib import request, error
+
+    try:
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
