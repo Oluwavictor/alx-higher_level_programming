7a0100ceb2d8bba7442e8d077b90217c80985ea2
Oluwavictor <46841607+Oluwavictor@users.noreply.github.com>
2023-05-03 21:33:05 +0100

    new commit

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,7 +1,7 @@
 const header = $('header');
 const toggleDiv = $('div#toggle_header');
 
-toggleClassElement(){
+function toggleClassElement(){
     header.toggleClass('red');
     header.toggleClass('green');
 };
